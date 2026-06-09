4a905ef2b02000105583441ba1bc494b24e0c9fe
zantoz50 <zantoz.office@gmail.com>
2026-06-09 23:01:20 +0545

    Inovice

--- src/routes/invoice.route.js
+++ src/routes/invoice.route.js
@@ -4,12 +4,22 @@ const invoiceController = require("../controllers/invoice.controller");
 const { requireAuth } = require("../middleware/auth.middleware");
 const tenantMiddleware = require("../middleware/tenant.middleware");
 
-router.use(requireAuth);
-router.use(tenantMiddleware);
+// router.use(requireAuth);
+// router.use(tenantMiddleware);
 
-router.post("/invoices", invoiceController.createInvoice);
+router.post(
+  "/invoices",
+  requireAuth,
+  tenantMiddleware,
+  invoiceController.createInvoice,
+);
 
 // GET /invoices
-router.get("/invoices", invoiceController.getInvoices);
+router.get(
+  "/invoices",
+  requireAuth,
+  tenantMiddleware,
+  invoiceController.getInvoices,
+);
 
 module.exports = router;
